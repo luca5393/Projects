e78065d1d8bccb8aaca43f5c62d8e3e946d14f7b
luca5393 <lucasvhp@outlook.dk>
2024-03-22 08:22:28 +0100

    Added kaostek and kajakklubben

--- Portfolio hjemmesiden/main.js
+++ Portfolio hjemmesiden/main.js
@@ -1,24 +1,7 @@
-function showProjectDetails(projectName) {
-    document.getElementById('header').style.display = 'none';
-    document.getElementById('projects-container').style.display = 'none';
-    document.getElementById('details-container').style.display = 'block';
-    document.getElementById('project-title').innerText = projectName;
-    
-    switch (projectName) {
-      case 'KaosTek':
-        document.getElementById('project-details').innerText = 'Details of Project 1';
-        break;
-      case 'Kajakklubben':
-        document.getElementById('project-details').innerText = 'Details of Project 2';
-        break;
-      default:
-        document.getElementById('project-details').innerText = 'Details not available';
-    }
-  }
+function navigateToProject(projectUrl, clickedProject) {
+  clickedProject.style.transform = 'translateX(100vw)';
   
-  function goBack() {
-    document.getElementById('header').style.display = 'block';
-    document.getElementById('projects-container').style.display = 'block';
-    document.getElementById('details-container').style.display = 'none';
-  }
-  
+  setTimeout(function() {
+    window.location.href = 'projects/' + projectUrl;
+  }, 500);
+}
